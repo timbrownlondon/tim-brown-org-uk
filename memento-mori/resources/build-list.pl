e6f971f85720b3feb343c621e03a6cee30a73777
Tim Brown <tim.brown.london@gmail.com>
2024-09-27 08:46:30 +0000

    updated build-list.pl to manage images

--- memento-mori/resources/build-list.pl
+++ memento-mori/resources/build-list.pl
@@ -1,11 +1,13 @@
 #!/usr/bin/env perl
 #
-# cat src-people.txt | ./build-list.pl > people.txt
+# cat people.txt | ./build-list.pl > people.html
 use strict;
 
 while (<>){
   chomp;
-  my($name, $birth_year, $death_year, $age) = split ',', $_;
+  my($name, $birth_year, $death_year, $age, $image_id) = split ',', $_;
 
-  print "$name<br>$birth_year - $death_year<br>$age years\n";
+  print "<h1>$name<br>$birth_year - $death_year<br>$age years</h1>";
+  print "<img src=\"https://s.tim-brown.org.uk/$image_id/large.jpg\" class=\"responsive\">" if $image_id; 
+  print "\n";
 }
