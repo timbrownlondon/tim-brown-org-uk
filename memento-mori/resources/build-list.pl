04d4aa9dee4e5041ec9a490e3b3a3117aa38e4a7
Tim Brown <tim.brown.london@gmail.com>
2024-09-28 16:38:47 +0000

    modified Memento Mori to use pipe separated data

--- memento-mori/resources/build-list.pl
+++ memento-mori/resources/build-list.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/env perl
 #
-# cat people.txt | ./build-list.pl > people.html
+# cat items.psv | ./build-list.pl > items.html
 use strict;
 
 while (<>){
   chomp;
-  my($name, $birth_year, $death_year, $age, $image_id) = split ',', $_;
+  my($image_id, $name, $birth_year, $death_year, $age) = split /\|/, $_;
 
-  print "<h1>$name<br>$birth_year - $death_year<br>$age years</h1>";
+  print "<h1>$name<br>$birth_year - $death_year<br>$age years</h1>$image_id";
   print "<img src=\"https://s.tim-brown.org.uk/$image_id/large.jpg\" class=\"responsive\">" if $image_id; 
   print "\n";
 }
