4d169f86bfcc1224044647d29baeb75b54fbb56c
Tim Brown <tim.brown.london@gmail.com>
2024-10-08 17:25:58 +0000

    added support for managing maps with nginx vars

---
+++ maps/resources/build-vars.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+#
+# cat maps.psv | ./build-vars.pl > vars-for-all-pages.html
+use strict;
+
+while (<>){
+  chomp;
+  my($image_id, $title, $desc) = split /\|/, $_;
+
+  print "<!--#set var=\"ID\" value=\"$image_id\"-->" if $image_id;
+  print "<!--#set var=\"TITLE\" value=\"$title\"-->" if $title;
+  print "<!--#set var=\"DESC\" value=\"$desc\"-->" if $desc;
+  print "\n";
+}
