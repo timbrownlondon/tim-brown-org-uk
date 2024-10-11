2ee7e062dd7e7877a9f4d9076dfb81c1f6472f0a
Tim Brown <tim.brown.london@gmail.com>
2024-10-11 15:43:39 +0000

    modified memento-mori to use selected.html and total.html

---
+++ memento-mori/resources/build-vars-files.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+#
+# usage:
+# cat resources/data.psv | resources/build-vars.pl
+#
+# this writes a set of vars files in the working directory
+
+use strict;
+my $n = 1;
+
+while (<>){
+  chomp;
+  my($image_id, $extension, $title, $born, $died, $age) = split /\|/, $_;
+
+  my $file = 'vars-'.$n++.'.html';
+  print "Writing to $file\n";
+
+  open my $out, '>', $file or die $!;
+  print $out "<!--#set var=\"ID\" value=\"$image_id\"-->" if $image_id;
+  print $out "<!--#set var=\"EXT\" value=\"$extension\"-->" if $extension;
+  print $out "<!--#set var=\"TITLE\" value=\"$title\"-->" if $title;
+  print $out "<!--#set var=\"BORN\" value=\"$born\"-->";
+  print $out "<!--#set var=\"DIED\" value=\"$died\"-->";
+  print $out "<!--#set var=\"AGE\" value=\"$age\"-->";
+  print $out "\n";
+  close $out
+}
