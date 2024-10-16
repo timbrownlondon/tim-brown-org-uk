2d42b82f994c070eb1ba1dc1ab1dae3251d6a2c1
Tim Brown <tim.brown.london@gmail.com>
2024-10-16 16:26:31 +0000

    added catalog/ to Coast

---
+++ coast/resources/build-list.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+#
+# cat resources/data.psv | resources/build-list.pl > catalog/list.html
+
+use strict;
+
+while (<>){
+  chomp;
+  my($image_id, $title, $description) = split /\|/, $_;
+
+  print "<b>$title</b><br>$description<br>";
+  print "<img src=\"https://s.tim-brown.org.uk/$image_id/small.jpg\"><hr>\n";
+}
