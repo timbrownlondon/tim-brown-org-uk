d49a7e19b6af7d4a9b383062d3fa04195426e7d0
Tim Brown <tim.brown.london@gmail.com>
2024-10-21 17:01:03 +0000

    added DESC field to Memento Mori data

--- memento-mori/resources/build-vars-files.pl
+++ memento-mori/resources/build-vars-files.pl
@@ -10,7 +10,7 @@ my $n = 1;
 
 while (<>){
   chomp;
-  my($image_id, $extension, $title, $born, $died, $age) = split /\|/, $_;
+  my($image_id, $extension, $title, $born, $died, $age, $desc) = split /\|/, $_;
 
   next unless $image_id;
 
@@ -18,12 +18,13 @@ while (<>){
   print "Writing to $file\n";
 
   open my $out, '>', $file or die $!;
-  print $out "<!--#set var=\"ID\" value=\"$image_id\"-->" if $image_id;
-  print $out "<!--#set var=\"EXT\" value=\"$extension\"-->" if $extension;
-  print $out "<!--#set var=\"TITLE\" value=\"$title\"-->" if $title;
+  print $out "<!--#set var=\"ID\" value=\"$image_id\"-->";
+  print $out "<!--#set var=\"EXT\" value=\"$extension\"-->";
+  print $out "<!--#set var=\"TITLE\" value=\"$title\"-->";
   print $out "<!--#set var=\"BORN\" value=\"$born\"-->";
   print $out "<!--#set var=\"DIED\" value=\"$died\"-->";
   print $out "<!--#set var=\"AGE\" value=\"$age\"-->";
+  print $out "<!--#set var=\"DESC\" value=\"$desc\"-->";
   print $out "\n";
   close $out
 }
