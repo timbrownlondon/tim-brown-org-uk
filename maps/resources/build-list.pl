2a7de67cefdccb98a52014a939bc25bb06cc51e4
Tim Brown <tim.brown.london@gmail.com>
2024-10-23 20:55:52 +0000

    fixed build-list.pl to ignore blank lines in data

--- maps/resources/build-list.pl
+++ maps/resources/build-list.pl
@@ -7,6 +7,7 @@ use strict;
 while (<>){
   chomp;
   my($image_id, $title, $description) = split /\|/, $_;
+  next unless $image_id;
 
   print "<b>$title</b><br>$description<br>";
   print "<img src=\"https://s.tim-brown.org.uk/$image_id/small.jpg\"><hr>\n";
