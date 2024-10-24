26103ed9fb8c470df5f8b912fe8b50db181b4167
Tim Brown <tim.brown.london@gmail.com>
2024-10-24 17:21:58 +0000

    updated layout of catalog pages

--- maps/resources/build-list.pl
+++ maps/resources/build-list.pl
@@ -9,6 +9,5 @@ while (<>){
   my($image_id, $title, $description) = split /\|/, $_;
   next unless $image_id;
 
-  print "<b>$title</b><br>$description<br>";
-  print "<img src=\"https://s.tim-brown.org.uk/$image_id/small.jpg\"><hr>\n";
+  print "<p>$title</p><img src=\"https://s.tim-brown.org.uk/$image_id/small.jpg\"><hr>\n";
 }
