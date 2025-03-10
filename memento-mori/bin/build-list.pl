131eb9e4d7fc384de950065f1425facf70289e90
Tim Brown <tim.brown.london@gmail.com>
2025-03-10 17:32:11 +0000

    updated Memento Mori to remove 't/' from image id data

--- memento-mori/bin/build-list.pl
+++ memento-mori/bin/build-list.pl
@@ -8,5 +8,5 @@ while (<>){
   my($image_id, $ext, $name, $birth_year, $death_year, $age) = split /\|/, $_;
   next unless $image_id;
 
-  print "<p>$name</p><img src=\"https://s.tim-brown.org.uk/$image_id/small.jpg\"><hr>\n";
+  print "<p>$name</p><img src=\"https://s.tim-brown.org.uk/t/$image_id/small.jpg\"><hr>\n";
 }
