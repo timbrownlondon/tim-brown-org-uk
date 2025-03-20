816ed1b7c4a94968ce6ba7f15dd9c39418a8106c
Tim Brown <tim.brown.london@gmail.com>
2025-03-20 22:02:08 +0000

    added update-collections.sh to remake image-list files for collections

---
+++ bin/update-collections.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+
+for COLLECTION in maps letters-numbers railway london coast body memento-mori
+do
+  DATA=$COLLECTION/ids.txt
+  wc -l $DATA
+  cat $DATA | bin/write-image-list.pl  > $COLLECTION/image-list.html
+done
