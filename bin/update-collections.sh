2498c1cae28ceb7be0a2f7203a80463d9817662e
Tim Brown <tim.brown.london@gmail.com>
2025-03-27 08:41:41 +0000

    renamed Railway -> Railways

--- bin/update-collections.sh
+++ bin/update-collections.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-for COLLECTION in maps letters-numbers railway london coast body memento-mori journal gallery
+for COLLECTION in maps letters-numbers railways london coast body memento-mori journal gallery childrens-art
 do
   DATA=$COLLECTION/ids.txt
   wc -l $DATA
