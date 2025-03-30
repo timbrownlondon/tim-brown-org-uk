83276c0f01042a4d60a714b732026faf1fea3282
Tim Brown <tim.brown.london@gmail.com>
2025-03-30 13:12:44 +0000

    updated write-flickr-page.pl to be aware of mapping.csv

--- bin/write-flickr-page.pl
+++ bin/write-flickr-page.pl
@@ -1,10 +1,23 @@
 #!/bin/env perl
 
+use strict;
+my $map;
+
+open my $in, '<', 'mapping.csv' or die $!;
+while (<$in>){
+  chomp;
+  my ($flickr, $id) = split ',';
+  $map->{$flickr} = $id;
+}
+close $in;
+
 while (<>){
   chomp;
   my ($id, $title, $url, $date) = split '\|';
 
   print '<img src="', $url, '"><br>';
+  print '<b>', $map->{$id}, '</b> ' if $map->{$id};
+  print '(', $id, ')<br>';
   print $title, '<br>';
   print $date, "<br>\n";
 }
