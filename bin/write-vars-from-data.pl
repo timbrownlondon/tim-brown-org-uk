b7dd7774201d75821f6ef3ee305aff3a95547e3f
Tim Brown <tim.brown.london@gmail.com>
2025-03-31 16:02:20 +0000

    updated page template, ssi files, and write-vars-from-data.pl to show collection membership on image page

--- bin/write-vars-from-data.pl
+++ bin/write-vars-from-data.pl
@@ -1,23 +1,45 @@
 #!/usr/bin/env perl
 use strict;
+use Data::Dumper;
 
-# usage:
-# cat img-data.psv | bin/write-vars-from-data.pl
+# map collection membership
+my $collection_members;
 
+for my $F (glob '*/ids.txt'){
+  my ($collection_dir) = $F =~m|(.*?)/|;
+
+  open my $ids_fh, '<', $F or die $!;
+  for my $id (<$ids_fh>){
+    chomp $id;
+    $collection_members->{$id}->{$collection_dir}++;
+  }
+  close $ids_fh;
+}
+
+# ingest image data
 open my $data_file, '<', 'img-data.psv' or die $!;
 my @data = <$data_file>;
 close $data_file;
 
+
+# write an ssi file for each image
 for (@data){
   chomp;
-  my ($img_id, $title, $desc, $ext) = split '\|';
+  my ($id, $title, $desc, $ext) = split '\|';
 
-  my $ssi_file = "ssi/$img_id.html";
-  my $content = '<!--#set var="IMG_ID" value="' . $img_id . '"-->' .
+  my $ssi_file = "ssi/$id.html";
+  my $content = '<!--#set var="IMG_ID" value="' . $id . '"-->' .
                 '<!--#set var="TITLE" value="' . $title . '"-->' .
                 '<!--#set var="DESC" value="' . $desc . '"-->';
 
   $content .= '<!--#set var="EXT" value="' . $ext . '"-->' if $ext;
+
+  for my $C (sort keys %{$collection_members->{$id}}){
+    $C =~ s/[^\w]//g;
+    $content .= '<!--#set var="';
+    $content .= uc $C;
+    $content .= '" value="TRUE"-->';
+  }
 
   open my $outfile, '>', $ssi_file or die $!;
   print  $outfile $content;
