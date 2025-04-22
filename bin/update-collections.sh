769929209fe1bec56d3d4786aad4e15418c6ac37
Tim Brown <tim.brown.london@gmail.com>
2025-04-22 14:42:57 +0000

    modified bin/update-collections.sh

--- bin/update-collections.sh
+++ bin/update-collections.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/sh
 
-for DATA in  ls */ids.txt
+for DATA in $(ls */ids.txt)
 do
   wc -l $DATA
   COLLECTION=$(dirname $DATA)
-  shuf $DATA | bin/write-image-list.pl  > $COLLECTION/image-list.html
+  shuf $DATA | bin/write-image-list.pl > $COLLECTION/image-list.html
 done
