461969bd103f17ae61faed4f030f5536cfd503f8
Tim Brown <tim.brown.london@gmail.com>
2025-05-10 06:37:27 +0000

    modified update-list-pages.sh to shuffle picture order

--- bin/update-list-pages.sh
+++ bin/update-list-pages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 bin/list-medium-images-in-s3.sh
-cat list/medium-images.txt | bin/split-image-list.pl 
+shuf list/medium-images.txt | bin/split-image-list.pl 
 bin/write-list-html.pl
 
