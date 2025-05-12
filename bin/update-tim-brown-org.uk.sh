d38fb256500c1774889073a8d40419fbae345020
Tim Brown <tim.brown.london@gmail.com>
2025-05-12 19:05:50 +0000

    modified update-tim-brown-org.uk.sh to also shiffle list pages

--- bin/update-tim-brown-org.uk.sh
+++ bin/update-tim-brown-org.uk.sh
@@ -9,3 +9,7 @@ bin/update-daily-selection.pl > daily-selection.html 2>> $LOG
 
 # shuffle contents of collection pages
 bin/update-collections.sh
+
+# shuffle contents of list pages
+shuf list/medium-images.txt | bin/split-image-list.pl
+bin/write-list-html.pl
