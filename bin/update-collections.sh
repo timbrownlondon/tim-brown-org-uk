49bda7abac2f617eb4abc441033d9043494f6478
Tim Brown <tim.brown.london@gmail.com>
2026-03-10 22:01:56 +0000

    updated update-collections.sh to preserve the first id when encounter/ids.txt is shuffled

--- bin/update-collections.sh
+++ bin/update-collections.sh
@@ -4,6 +4,11 @@ for DATA in $(ls */ids.txt)
 do
   wc -l $DATA
   COLLECTION=$(dirname $DATA)
-  shuf $DATA > $DATA.shuffled
+  if [ "$COLLECTION" = "encounter" ]; then
+    { head -1 $DATA; tail -n +2 $DATA | shuf; } > $DATA.shuffled
+  else
+    shuf $DATA > $DATA.shuffled
+  fi
   cat $DATA.shuffled | bin/write-image-list.pl > $COLLECTION/image-list.html
 done
+
