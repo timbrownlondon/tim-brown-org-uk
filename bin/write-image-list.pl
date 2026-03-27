e555ddf7e69801536340b2bd48f243a790597cb5
Tim Brown <tim.brown.london@gmail.com>
2026-03-27 07:33:55 +0000

    modified write-image-list.pl to include alt

--- bin/write-image-list.pl
+++ bin/write-image-list.pl
@@ -1,10 +1,19 @@
 #!/bin/env perl
 
+use lib 'lib';
+
+use ArtWorks;
+
+my $AW = ArtWorks->new_from_file('img-data.psv');
+
 my $url = 'https://s.tim-brown.org.uk/t';
 
 while (<>){
   chomp;
+  my $title =  $AW->title($_);
+  die $title if $title =~ m/"/;
+
   print '<a href="/img/', $_, '">';
-  print '<img src="', $url, '/', $_, '/medium.jpg">';
+  print '<img src="', $url, '/', $_, '/medium.jpg" alt="', $title, '">';
   print "</a>\n";
 }
